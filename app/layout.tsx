c5ba910a0c4c9c169f2caf51d82e8f333eff0254
niclundegard <nlundegard@proton.me>
2026-04-17 21:48:02 -0400

    Fixing the umami tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Nav from "@/components/nav";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: "Lundy Labs LLC",
@@ -18,10 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
-      <head>
-        <script defer src="https://cloud.umami.is/script.js" data-website-id="02226505-26bb-4a1d-a7d0-92b23a42d8c5"></script>
-      </head>
       <body>
+        <Script 
+          defer 
+          src="https://cloud.umami.is/script.js" 
+          data-website-id="02226505-26bb-4a1d-a7d0-92b23a42d8c5"
+        />
         <Nav />
         {children}
       </body>
